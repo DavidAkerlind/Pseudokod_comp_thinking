cc6a6ee222198d56967e749e72de28a1ac2b52e8
DavidAkerlind <david.akerlind@icloud.com>
2024-12-16 13:00:23 +0100

    test med pseudokod

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 /*
+Knock Out 
 
+let Uservalue  // låt användaren välja knockoutnumer 
 
+OM (userValue > 5 och < 10)
+    userValue == knockoutnumber
+ANNARS 
+    "Ogiltigt värde // välj igen" 
+OM end
 
+let vinstNummer // låt användaren välja vinstnummer
 
+let totalPoäng == 0
 
+MEDAN totalPoäng inte är lika med vinstNummer // totalPoäng !== vinstNummer
+    tärning1 == random nummer mellan 1 och 6
+    tärning2 == random nummer mellan 1 och 6 
 
+    tärning1 + tärning2 == score
+
+    OM score !== knockoutnummer
+        totalPoäng == totalpoäng + score
+    ANNARS
+        totalPoäng == totalpoäng - score
+    OM end
+MEDAN end
+
+" Du vann! "
 
 */
